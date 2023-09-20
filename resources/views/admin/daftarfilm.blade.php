d17c56268a1806095d848e80841b29d517c29e69
lyhlmtss30 <ilyahalimatussadyah@gmail.com>
2023-09-20 14:43:28 +0700

    fix daftar film

--- resources/views/admin/daftarfilm.blade.php
+++ resources/views/admin/daftarfilm.blade.php
@@ -2,45 +2,83 @@
 
 @section('content')
     <style>
-        body {
-          font-family: Arial, sans-serif; /* Ganti dengan jenis font yang Anda sukai */
-          background-color: rgb(78, 73, 72);
-      }
-      .text {
-          padding-left: 100px;
-          color: #ffffff; /* Ganti dengan warna yang Anda sukai */
-          margin-top: 40px;
-      }
-      h1 {
-          color: #fff6f6; /* Warna judul */
+         body {
+        font-family: Arial, sans-serif;
+        background-color: rgb(78, 73, 72);
+    }
 
-      }
+    .text {
+        padding-left: 100px;
+        color: #ffffff;
+        margin-top: 40px;
+    }
 
-      .p{
-          width: 300px;
-          font-family:Arial, Helvetica, sans-serif;
-      }
-      .table-container {
-            margin-top: 40px;
-            text-align: right; /* Menggeser tabel ke kanan */
-        }
+    h1 {
+        color: #fff6f6;
+    }
+
+    .p {
+        width: 300px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .table-container {
+        margin-top: 200px;
+
+    }
+
+    /* Style tabel sesuai kebutuhan Anda */
+    .table {
+        width: 90%;
+        height: 20%;
+        color: #ffffff;
+    }
+
+    .table th,
+    .table td {
+        text-align: center;
+        padding: 5px;
+    }
+
+    .table th {
+        background-color: #333;
+    }
+
+    .table tr:nth-child(even) {
+        background-color: #444;
+    }
+
+    .table tr:nth-child(odd) {
+        background-color: #555;
+    }
+
+    .table th, .table td {
+        border: 1px solid #777;
+    }
+
+    .table img {
+        max-width: 90px;
+    }
+
+    .button {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    .button button {
+        background-color: #333;
+        color: #ffffff;
+        border: none;
+        padding: 5px 10px;
+        cursor: pointer;
+    }
+
+    .button button:hover {
+        background-color: #555;
+    }
 
 
-      /* Style tabel sesuai kebutuhan Anda */
-      .table {
-          width: 83%;
-          margin-top: 150px; /* Jarak antara tabel dan p */
-          margin-left: 0;
-          color: #ffffff; /* Warna teks dalam tabel */
-      }
-
-      .th, .td {
-          text-align: center;
-          padding: 8px;
-      }
-      .td button {
-        margin-bottom: 100px; /* Sesuaikan nilai margin sesuai kebutuhan Anda */
-}
       </style>
 </head>
 
@@ -72,7 +110,7 @@
             @forelse ( $films as $i=> $film )
 
             <tr>
-                <td>{{ $i+1 }}</td>
+                <td>{{ $i+1 }} </td>
                 <td>{{ $film->judul }}</td>
                 <td>{{ $film->director }}</td>
                 <td>{{ $film->cast }}</td>
@@ -80,11 +118,13 @@
                 <td>{{ $film->genre }}</td>
                 <td>{{ $film->durasi }}</td>
                 <td>{{ $film->jadwal_tayang }}</td>
-                <td>{{ $film->sinopsis }}</td>
+                <td>
+                    <p class="overflow-y-scroll" rows="3">{{ $film->sinopsis }}</p>
+                </td>
                 <td><img src="{{ asset('storage/thumbnile/' . $film->thumbnile ) }}" alt="{{ $film->judul }}"></td>
                 <div class="button">
                     <td>
-                        <button class="btn btn-dark" onclick="return confirm('Apakah anda yakin?')">Edit</button>
+                        <button class="btn btn-dark">Edit</button>
 
                         <button class="btn btn-dark" onclick="return confirm('Apakah anda yakin?')">Hapus</button>
                     </td>
