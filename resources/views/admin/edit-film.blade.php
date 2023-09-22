d36bf455b9d13a6d118ad992df127f459b281bdb
lyhlmtss30 <ilyahalimatussadyah@gmail.com>
2023-09-22 08:18:43 +0700

    edit film

--- resources/views/admin/edit-film.blade.php
+++ resources/views/admin/edit-film.blade.php
@@ -1,30 +1,35 @@
 @extends('layouts.admin')
-
- <head>
-    {{-- <link rel="stylesheet" href="{{create-detailcomingsoon.css}}"> --}}
-    <link rel="stylesheet" href="{{ URL::asset('css/admin/create-film.css') }}">
-    {{-- @include('layouts.cdn') --}}
-
-</head>
+       
+    @section('content')
 
 
+        @if (session('success'))
+            <script>
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Upload berhasil',
+                    showConfirmButton: false,
+                    timer: 2000
+                })
+            </script>
+        @endif
 
-@section('content')
+<style>
+    body {
+    background-color: #333; /* Warna latar belakang lebih gelap */
+    color: #fff; /* Warna teks lebih cerah */
+}
+.input-text {
+    background-color: #444;
+    color: #fff;
+    /* Tambahkan efek hover untuk input */
+}
 
-
-    @if (session('success'))
-        <script>
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'Upload berhasil',
-                showConfirmButton: false,
-                timer: 2000
-            })
-        </script>
-    @endif
-
-
+.input-file {
+    background-color: #555;
+    color: #fff;
+}
 
     <div class="container col-7 mt-4">
         <h1 style="font-family: 'Poppins', sans-serif; font-weight: bold; text-align: center; color:#fff;" >Edit Film</h1>
@@ -51,24 +56,59 @@
                 </div>
         </div>
 
-        <div class="form-container col-6 ">
-            <div class="border-form p-3">
+</style>
 
-                <input type="text" id="durasi" name="durasi" class="input-text" placeholder="Durasi"
-                required value="{{ $film[0]->durasi }}" >
-                <input type="date" id="jadwal_tayang" name="jadwal_tayang" class="input-text"
-                placeholder="Jadwal Tayang"  value="{{ $film[0]->jadwal_tayang }}" >
-
-                <input type="url" id="trailer" name="trailer" class="input-text"
-                placeholder="Link Video Trailer" value="{{ $film[0]->trailer }}"  >
-                <textarea name="sinopsis" id="sinopsis" class="input-text" cols="30" rows="5" placeholder="Sinopsis">
-                    {{ $film[0]->sinopsis }}
-                </textarea>
-<br><br>
+        <div class="container col-7 mt-4">
+            <h1 style="font-family: 'Poppins', sans-serif; font-weight: bold; text-align: center; color:#fff;" >Edit Film</h1>
+            <div class="row">
+            <div class="form-container col-6 ">
+                <div class="border-form ">
+                <form action="{{ route('film.update',$film[0]->id) }}" method="POST" enctype="multipart/form-data">
+                <!-- Form Tambah Data Film (Form Pertama) -->
+                    @csrf <!-- Ini digunakan di Laravel untuk melindungi dari serangan CSRF -->
+                        <input type="text" id="judul" name="judul" class="input-text" placeholder="Judul" required>
+                        <input type="text" id="director" name="director" class="input-text" placeholder="Director"
+                        required>
+                        <input type="text" id="cast" name="cast" class="input-text" placeholder="Cast">
+                        <input type="number" id="minimal_usia" name="minimal_usia" class="input-text"
+                        placeholder="Minimal Usia" min="0" value="{{ $film[0]->minimal_usia }}" >
+                        <input type="text" id="genre" name="genre" class="input-text" placeholder="Genre" value="{{ $film[0]->genre }}" >
+                        <select name="status" id="status" class="input-text" required>
+                            <option value="" disabled >Pilih</option>
+                            <option value="nowplaying" @if($film[0]->status ==="nowplaying") selected @endif>Now Playing</option>
+                            <option value="commingsoon" @if($film[0]->status ==="commingsoon") selected @endif >Coming Soon</option>
+                        </select>
+                    </div>
             </div>
+
+            <div class="form-container col-6 ">
+                <div class="border-form p-3">
+
+                    <input type="text" id="durasi" name="durasi" class="input-text" placeholder="Durasi"
+                    required value="{{ $film[0]->durasi }}" >
+                    <input type="date" id="jadwal_tayang" name="jadwal_tayang" class="input-text"
+                    placeholder="Jadwal Tayang"  value="{{ $film[0]->jadwal_tayang }}" >
+
+                    <input type="url" id="trailer" name="trailer" class="input-text"
+                    placeholder="Link Video Trailer" value="{{ $film[0]->trailer }}"  >
+                    <textarea name="sinopsis" id="sinopsis" class="input-text" cols="30" rows="5" placeholder="Sinopsis">
+                        {{ $film[0]->sinopsis }}
+                    </textarea>
+    <br><br>
+                </div>
+            </div>
+            <center>
+            <br><br>
+
+                <img src="{{ asset('storage/thumbnile/' . $film[0]->thumbnile) }}" alt="{{ $film[0]->judul }}">
+
+                <input type="file" id="thumbnile" name="thumbnile" accept="image/*" class="input-file col-8" required style="width: 10%;">
+
+                <button type="submit" class="input-submit" style="width: 90%;">Edit</button>
+            </center>
+        </form>
         </div>
-        <center>
-        <br><br>
+        <!-- Input File dan Tombol Submit di luar form -->
 
         <img src="{{ asset('storage/thumbnile/' . $film[0]->thumbnile) }}" alt="{{ $film[0]->judul }}" class="edit-image-frame">
 
@@ -79,9 +119,8 @@
         </center>
     </form>
     </div>
-    <!-- Input File dan Tombol Submit di luar form -->
-
-</div>
+</body>
+</html>
 @endsection
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,6 +21,10 @@
     {{-- ICON --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
+ {{-- <link rel="stylesheet" href="{{create-detailcomingsoon.css}}"> --}}
+ <link rel="stylesheet" href="{{ URL::asset('css/admin/create-film.css') }}">
+ {{-- @include('layouts.cdn') --}}
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
