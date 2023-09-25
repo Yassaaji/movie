0a2b9dfdaa39b1b7cfacc46819d6b1f2c68c7012
Syahla0519 <syahlatika19@gmail.com>
2023-09-24 21:49:40 -0700

    update create film

--- resources/views/admin/create-film.blade.php
+++ resources/views/admin/create-film.blade.php
@@ -1,83 +1,77 @@
 @extends('layouts.admin')
 
- <head>
-    {{-- <link rel="stylesheet" href="{{create-detailcomingsoon.css}}"> --}}
-    <link rel="stylesheet" href="{{ URL::asset('css/admin/create-film.css') }}">
-    {{-- @include('layouts.cdn') --}}
+@section('content')
+<!DOCTYPE html>
+<html>
 
+<head>
+    <title>Create Film</title>
+    <!-- Tambahkan referensi ke CSS Bootstrap dan CSS khusus Anda -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ URL::asset('css/admin/create-film.css') }}">
 </head>
 
+<body>
+    <div class="container mt-4">
+        <h1 class="text-center" style="font-family: 'Poppins', sans-serif; font-weight: bold; color:#fff;">Create Film</h1>
+        <div class="row">
+            <div class="col-4">
+                <div class="border-form">
+                    <form action="{{ route('uploadfilm') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
 
-
-@section('content')
-
-
-@if (session('success'))
-<script>
-    Swal.fire({
-    position: 'center',
-  icon: 'success',
-  title: 'Upload berhasil',
-  showConfirmButton: false,
-  timer: 2000
-})
-</script>
-@endif
-
-<div class="container col-7 mt-4">
-    <h1 style="font-family: 'Poppins', sans-serif; font-weight: bold; text-align: center; color:#fff;" >Create Film</h1>
-    <div class="row">
-        <div class="form-container col-6 ">
-            <div class="border-form ">
-            <form action="{{ route('uploadfilm') }}" method="POST" enctype="multipart/form-data">
-            <!-- Form Tambah Data Film (Form Pertama) -->
-                @csrf <!-- Ini digunakan di Laravel untuk melindungi dari serangan CSRF -->
-
-                    <input type="text" id="judul" name="judul" class="input-text" placeholder="Judul" required>
-                    <input type="text" id="director" name="director" class="input-text" placeholder="Director"
-                    required>
-                    <input type="text" id="cast" name="cast" class="input-text" placeholder="Cast">
-                    <input type="number" id="minimal_usia" name="minimal_usia" class="input-text"
-                    placeholder="Minimal Usia" min="0">
-                    <input type="text" id="genre" name="genre" class="input-text" placeholder="Genre">
-                    <select name="status" id="status" class="input-text" required>
-                        <option value="" disabled selected>Pilih</option>
-                        <option value="nowplaying">Now Playing</option>
-                        <option value="commingsoon">Coming Soon</option>
-                    </select>
-
+                        <input type="text" id="judul" name="judul" class="input-text" placeholder="Judul" required>
+                        <input type="text" id="director" name="director" class="input-text" placeholder="Director" required>
+                        <input type="text" id="cast" name="cast" class="input-text" placeholder="Cast">
+                        <input type="number" id="minimal_usia" name="minimal_usia" class="input-text"
+                            placeholder="Minimal Usia" min="0">
+                        <input type="text" id="genre" name="genre" class="input-text" placeholder="Genre">
+                        <select name="status" id="status" class="input-text" required>
+                            <option value="" disabled selected>Pilih</option>
+                            <option value="nowplaying">Now Playing</option>
+                            <option value="comingsoon">Coming Soon</option>
+                        </select>
+                    </div>
                 </div>
-        </div>
 
-        <div class="form-container col-6 ">
-            <div class="border-form p-3">
+                <div class="col-4">
+                    <div class="border-form p-3">
+                        <input type="text" id="durasi" name="durasi" class="input-text" placeholder="Durasi" required>
+                        <input type="date" id="jadwal_tayang" name="jadwal_tayang" class="input-text"
+                            placeholder="Jadwal Tayang" required>
+                        <input type="url" id="trailer" name="trailer" class="input-text"
+                            placeholder="Link Video Trailer" required>
+                        <textarea name="sinopsis" id="sinopsis" class="input-text" cols="30" rows="3"
+                            placeholder="Sinopsis"></textarea>
+                    </div>
+                </div>
 
-                <input type="text" id="durasi" name="durasi" class="input-text" placeholder="Durasi"
-                required>
-                <input type="date" id="jadwal_tayang" name="jadwal_tayang" class="input-text"
-                placeholder="Jadwal Tayang" required>
-                <input type="time" id="jam_tayang" name="jam_tayang" class="input-text"
-                placeholder="Jam Tayang" required>
-                <input type="url" id="trailer" name="trailer" class="input-text"
-                placeholder="Link Video Trailer" required>
-                <textarea name="sinopsis" id="sinopsis" class="input-text" cols="30" rows="5" placeholder="Sinopsis"></textarea>
-<br><br>
-
+                <div class="col-4">
+                    <div class="border-form p-3">
+                        <h3 style="font-family: 'Poppins', sans-serif; font-weight: bold; color:#000000;">Tiket</h3>
+                        <label for="ruangan">Ruangan:</label> <br>
+                        <select name="ruangan" id="ruangan">
+                            <option value="A">Ruang A</option>
+                            <option value="B">Ruang B</option>
+                            <option value="C">Ruang C</option>
+                        </select>
+                        <input type="time" id="jam_tayang" name="jam_tayang" class="input-text"
+                            placeholder="Jam Tayang" >
+                        {{-- <input type="text" id="kursi" name="kursi" class="input-text" placeholder="Kursi" > --}}
+                        <input type="number" id="harga" name="harga" class="input-text" placeholder="Harga" min="0">
+                    </div>
+                </div>
             </div>
-        </div>
-        <center>
-        <br><br>
 
-            <input type="file" id="thumbnile" name="thumbnile" accept="image/*" class="input-file col-8" required
-            style="width: 90%;">
-
-            <button type="submit" class="input-submit" style="width: 90%;">Upload</button>
-        </center>
-    </form>
+            <center>
+                <br><br>
+                <input type="file" id="thumbnail" name="thumbnail" accept="image/*" class="input-file " required
+                    style="width: 90%;">
+                <button type="submit" class="input-submit" style="width: 90%;">Upload</button>
+            </center>
+        </form>
     </div>
-    <!-- Input File dan Tombol Submit di luar form -->
+</body>
 
-</div>
+</html>
 @endsection
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- resources/views/admin/konfirmasi-ticket.blade.php
+++ resources/views/admin/konfirmasi-ticket.blade.php
@@ -11,9 +11,10 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
 
     <style>
-        /* Gaya untuk card */
+  
+
         .card {
-            background-color: rgb(0, 0, 0); /* Ubah warna latar belakang card menjadi navy */
+            background-color: #000000; /* Ubah warna latar belakang card menjadi navy */
             color: white; /* Ubah warna teks menjadi putih */
         }
 
@@ -35,120 +36,69 @@
             color: white; /* Ubah warna teks tombol menjadi putih */
             border: none; /* Hapus border tombol */
         }
-        
+
+        /* Gaya untuk tabel yang dapat digulir */
+        .fixed-table {
+            max-height: 300px; /* Tentukan tinggi maksimum untuk membuat tabel digulir jika perlu */
+            overflow-y: auto; /* Aktifkan gulir vertikal jika melebihi tinggi maksimum */
+        }
 
     </style>
 </head>
 
 <body>
+    
     <div class="container">
+             <div class="navbar-link-header pt-3 px-5">
+            <h3>Karyawan</h3>
+            <p>Konfirmasi Ticket</p>
+        </div>
         <div class="card">
             <div class="card-header">
-                <h1 class="text-center">Konfirmasi Pesanan Tiket</h1>
             </div>
             <div class="card-body">
                 <!-- Tombol Ruangan Teater -->
                 <div class="btn-group mt-3" role="group" aria-label="Ruangan Teater">
-                    <button type="button" class="btn btn-purple mr-2">XI</button>
-                    <button type="button" class="btn btn-purple mr-2">XII</button>
-                    <button type="button" class="btn btn-purple mr-2">XIII</button>
-                    <button type="button" class="btn btn-purple">XIV</button>
+                    <button type="button" class="btn btn-purple mr-3">A</button>
+                    <button type="button" class="btn btn-purple mr-3">B</button>
+                    <button type="button" class="btn btn-purple mr-3">C</button>
                 </div>
 
                 <!-- Tabel dengan gaya Bootstrap -->
-                
-                <table class="table table-dark mt-4  fixed-table">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Film</th>
-                            <th scope="col">Jam Tayang</th>
-                            <th scope="col">Ruangan</th>
-                            <th scope="col">Kursi</th>
-                            <th scope="col">Metode Pembayaran</th>
-                            <th scope="col">Total Harga</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Kakak ku ternyata manusia</td>
-                            <td>16.30</td>
-                            <td>XI</td>
-                            <td>2B</td>
-                            <td>ngutang</td>
-                            <td>35.000</td>
-                            <td>
-                                <button type="button" class="btn ">Konfirmasi</button>
-                                <button type="button" class="btn ">Gagal</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Kakak ku ternyata manusia</td>
-                            <td>16.30</td>
-                            <td>XI</td>
-                            <td>2B</td>
-                            <td>ngutang</td>
-                            <td>35.000</td>
-                            <td>
-                                <button type="button" class="btn ">Konfirmasi</button>
-                                <button type="button" class="btn ">Gagal</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Kakak ku ternyata manusia</td>
-                            <td>16.30</td>
-                            <td>XI</td>
-                            <td>2B</td>
-                            <td>ngutang</td>
-                            <td>35.000</td>
-                            <td>
-                                <button type="button" class="btn ">Konfirmasi</button>
-                                <button type="button" class="btn ">Gagal</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Kakak ku ternyata manusia</td>
-                            <td>16.30</td>
-                            <td>XI</td>
-                            <td>2B</td>
-                            <td>ngutang</td>
-                            <td>35.000</td>
-                            <td>
-                                <button type="button" class="btn ">Konfirmasi</button>
-                                <button type="button" class="btn ">Gagal</button>
-                            </td>
-                        </tr><tr>
-                            <th scope="row">1</th>
-                            <td>Kakak ku ternyata manusia</td>
-                            <td>16.30</td>
-                            <td>XI</td>
-                            <td>2B</td>
-                            <td>ngutang</td>
-                            <td>35.000</td>
-                            <td>
-                                <button type="button" class="btn ">Konfirmasi</button>
-                                <button type="button" class="btn ">Gagal</button>
-                            </td>
-                        </tr><tr>
-                            <th scope="row">1</th>
-                            <td>Kakak ku ternyata manusia</td>
-                            <td>16.30</td>
-                            <td>XI</td>
-                            <td>2B</td>
-                            <td>ngutang</td>
-                            <td>35.000</td>
-                            <td>
-                                <button type="button" class="btn ">Konfirmasi</button>
-                                <button type="button" class="btn ">Gagal</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="table-responsive fixed-table mt-4"> <!-- Tambahkan class .table-responsive -->
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Film</th>
+                                <th scope="col">Jam Tayang</th>
+                                <th scope="col">Ruangan</th>
+                                <th scope="col">Kursi</th>
+                                <th scope="col">Metode Pembayaran</th>
+                                <th scope="col">Total Harga</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Isi tabel -->
+                            <!-- Contoh baris tabel -->
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Kakak ku ternyata manusia</td>
+                                <td>16.30</td>
+                                <td>XI</td>
+                                <td>2B</td>
+                                <td>ngutang</td>
+                                <td>35.000</td>
+                                <td>
+                                    <button type="button" class="btn ">Konfirmasi</button>
+                                    <button type="button" class="btn ">Gagal</button>
+                                </td>
+                            </tr>
+                            <!-- Contoh baris tabel lainnya -->
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
